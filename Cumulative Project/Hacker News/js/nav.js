f158d405e8643654b9452b363b30a8ac0d9e39b6
Sunwoo Lee <slee2021@fau.edu>
2022-10-11 13:37:22 -0400

    Rough Draft of Hacker News Code

---
+++ Cumulative Project/Hacker News/js/nav.js
@@ -0,0 +1,122 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+   console.debug("navAllStories", evt);
+   hidePageComponents();
+   putStoriesOnPage();
+   localStorageFav();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+   console.debug("navLoginClick", evt);
+   hidePageComponents();
+   $loginForm.show();
+   $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+function profile() {
+   hidePageComponents();
+   $profileForm.show();
+}
+
+$("#nav-user-profile").on("click", profile);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+   console.debug("updateNavOnLogin");
+   $(".navbar-main").show();
+   $navLogin.hide();
+   $navLogOut.show();
+   $navUserProfile.text(`${currentUser.username}`).show();
+   $loginForm.hide();
+   $signupForm.hide();
+}
+
+function submitNewStory(evt) {
+   navAllStories();
+   $newStoryForm.show();
+}
+
+$navSubmit.on("click", submitNewStory);
+
+function onlyFavoriteStories() {
+   const favoriteStories = currentUser.favorites;
+   hidePageComponents();
+
+   if (favoriteStories.length === 0) $blankContainer.show().html("No Favorites Added!");
+   else {
+      $favorites.empty();
+
+      for (let story of favoriteStories) {
+         const $favStory = generateStoryMarkup(new Story(story));
+         $favorites.append($favStory);
+      }
+
+      $favorites.show();
+   }
+}
+
+$navFavorites.on("click", onlyFavoriteStories);
+
+function onlyUserStories() {
+   const userStories = currentUser.ownStories;
+   hidePageComponents();
+
+   if (userStories.length === 0) $blankContainer.show().html("No Stories Added by User!");
+   else {
+      $myStories.empty();
+
+      for (let story of userStories) {
+         const $userStory = generateUserStoryMarkup(story);
+         $myStories.append($userStory);
+      }
+      $myStories.show();
+   }
+   $myStories.on("click", ".delete-story", deleteUserStory);
+
+   $(".delete-story").hover(
+      function () {
+         $(this).parent().css("color", "red").css("text-decoration", "line-through");
+      },
+      function () {
+         $(this).parent().css("color", "").css("text-decoration", "none");
+      }
+   );
+}
+
+$navStories.on("click", onlyUserStories);
+
+async function deleteUserStory(e) {
+   const id = e.target.parentElement.id;
+   const token = currentUser.loginToken;
+   const ownStories = currentUser.ownStories;
+
+   const response = await axios({
+      url: `${BASE_URL}/stories/${id}`,
+      method: "DELETE",
+      data: { token },
+   });
+
+   for (let i = 0; i < ownStories.length; i++) {
+      if (ownStories[i].storyId === response.data.story.storyId) {
+         currentUser.ownStories.splice(i, 1);
+      }
+   }
+
+   $(this).parent().remove();
+
+   onlyUserStories();
+}
---
+++ Cumulative Project/Hacker News/js/stories.js
@@ -0,0 +1,146 @@
+"use strict";
+
+// This is the global list of the stories, an instance of StoryList
+let storyList;
+
+/** Get and show stories when site first loads. */
+
+async function getAndShowStoriesOnStart() {
+   storyList = await StoryList.getStories();
+   $storiesLoadingMsg.remove();
+
+   putStoriesOnPage();
+}
+
+/**
+ * A render method to render HTML for an individual Story instance
+ * - story: an instance of Story
+ *
+ * Returns the markup for the story.
+ */
+
+function generateStoryMarkup(story) {
+   // console.debug("generateStoryMarkup", story);
+
+   const hostName = story.getHostName();
+   return $(`
+      <li id="${story.storyId}">
+        <input class="star" type="checkbox">
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="story-user">posted by ${story.username}</small>
+      </li>
+    `);
+}
+
+function generateUserStoryMarkup(story) {
+   // console.debug("generateStoryMarkup", story);
+
+   const hostName = story.getHostName();
+   return $(`
+      <li id="${story.storyId}">
+        <input class="star" type="checkbox">
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="delete-story">[DELETE]</small>
+        <small class="story-user">posted by ${story.username}</small>
+      </li>
+    `);
+}
+
+/** Gets list of stories from server, generates their HTML, and puts on page. */
+
+function putStoriesOnPage() {
+   console.debug("putStoriesOnPage");
+
+   $allStoriesList.empty();
+
+   // loop through all of our stories and generate HTML for them
+   for (let story of storyList.stories) {
+      const $story = generateStoryMarkup(story);
+      $allStoriesList.append($story);
+   }
+
+   $allStoriesList.show();
+}
+
+function localStorageFav() {
+   if (localStorage.favorite) {
+      const $storiesLis = $allStoriesList.children();
+      const localFav = JSON.parse(localStorage.getItem("favorite"));
+
+      currentUser.favorites.splice(0);
+      for (let i = 0; i < localFav.length; i++) {
+         currentUser.favorites.push(localFav[i]);
+      }
+
+      for (let i = 0; i < $storiesLis.length; i++) {
+         for (let j = 0; j < localFav.length; j++) {
+            if ($storiesLis[i].id === localFav[j].storyId) {
+               $storiesLis[i].children[0].checked = true;
+            }
+         }
+      }
+   }
+}
+
+async function submitNewStory(e) {
+   const author = $("#newStory-author").val();
+   const title = $("#newStory-title").val();
+   const url = $("#newStory-url").val();
+   const token = currentUser.loginToken;
+
+   const submission = await axios({
+      url: `${BASE_URL}/stories`,
+      method: "POST",
+      data: { token, story: { author, title, url } },
+   });
+
+   currentUser.ownStories.push(new Story(submission.data.story));
+
+   getAndShowStoriesOnStart();
+
+   $newStoryForm.hide(500);
+}
+
+$newStorySubmit.on("click", submitNewStory);
+
+async function checkboxesForFavorite(e) {
+   const id = e.target.parentElement.id;
+   const username = currentUser.username;
+   const token = currentUser.loginToken;
+
+   if (e.target.checked) {
+      for (let i = 0; i < storyList.stories.length; i++) {
+         if (storyList.stories[i].storyId === id) {
+            const response = await axios({
+               url: `${BASE_URL}/users/${username}/favorites/${id}`,
+               method: "POST",
+               data: { token },
+            });
+            currentUser = response.data.user;
+         }
+      }
+   }
+   if (!e.target.checked) {
+      for (let i = 0; i < currentUser.favorites.length; i++) {
+         if (currentUser.favorites[i].storyId === id) {
+            const response = await axios({
+               url: `${BASE_URL}/users/${username}/favorites/${id}`,
+               method: "DELETE",
+               data: { token },
+            });
+            currentUser = response.data.user;
+         }
+      }
+   }
+   updateUserFavorites();
+}
+
+$stars.on("click", "input", checkboxesForFavorite);
